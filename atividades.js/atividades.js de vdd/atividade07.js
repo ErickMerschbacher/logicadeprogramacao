6e91f808e80a31c053b399cbecaf2e8d6f407c34
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2025-02-21 10:07:53 -0300

    nova edição da finalização

--- atividades.js/atividades.js de vdd/atividade07.js
+++ atividades.js/atividades.js de vdd/atividade07.js
@@ -13,3 +13,32 @@ var matriz = [
 ];
 console.table(matriz);
 console.log(matriz[0][0], "|", matriz[1][1], "|", matriz[2][2]);
+console.log("----------------------------Agora o código certo----------------------------------")
+var matriz = [
+    [3, 5, 8],
+    [1, 9, 2],
+    [7, 1, 4],
+  ];
+  for (let linha = 0; linha < matriz.length; linha++) {
+    for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+      linha == coluna &&
+      console.log(`Matriz [${linha}][${coluna}] = ${matriz[linha][coluna]}`);
+    }
+  }
+  //Dada a matriz abaixo, faça um programa em JavaScript que multiplique seus elementos por um valor x e exiba ao usuário a matriz antes e após
+  //multiplicar
+// |   3 5 8   |
+// |   1 9 2   |
+// |   7 1 4   |
+var x = 3
+var matriz = [
+    [3, 5, 8],
+    [1, 9, 2],
+    [7, 1, 4],
+]
+for (let x = 0; x < numeros.length; x++) {
+    numeros[x] *= 3;
+  }
+  console.log("vetor multiplicado por 3");
+  console.table(numeros);
+  
