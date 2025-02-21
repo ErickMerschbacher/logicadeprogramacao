94cdbb330fb48022315a5557af41382230e04593
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2025-02-21 08:47:22 -0300

    atividade 07 do Erick

---
+++ atividades.js/atividades.js de vdd/atividade07.js
@@ -0,0 +1,15 @@
+//*********************************************************************************************************************** */
+//                                    Lista de exercícicos 07
+//************************************************************************************************************************* */
+//Dada a matriz abaixo, faça um programa em JavaScript que exiba ao usuário os elementos da sua diagonal principal.
+
+// |   3 5 8   |
+// |   1 9 2   |
+// |   7 1 4   |
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+console.table(matriz);
+console.log(matriz[0][0], "|", matriz[1][1], "|", matriz[2][2]);
